1947e6c84dc35f918958f071b4c2abe243a2aeb8
Kevin Urban <theonlyulti@gmail.com>
2012-09-11 12:01:53 +0200

    removed factory-girl from Gemfile and test directory
    should run on ruby 1.8 now

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source :rubygems
 group :development, :test do
   gem 'simplecov'
   gem 'simplecov-rcov'
-  gem 'factory_girl_rails'
   gem 'shoulda-matchers'
 end
--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'simplecov'
-require 'simplecov-rcov'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-SimpleCov.start 'rails' if ENV["COVERAGE"]
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-require File.expand_path(File.dirname(__FILE__) + '/factories')
